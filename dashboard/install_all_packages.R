0844e01566fcbfedb6061789f8a4a97f3e2d1294
codehering <64325723+codehering@users.noreply.github.com>
2021-07-10 16:24:09 +0200

    update

--- dashboard/install_all_packages.R
+++ dashboard/install_all_packages.R
@@ -1,3 +1,3 @@
 #this files installes all necessary packages for R shiny dashboard
 
-install.packages(c("shiny", "shinydashboard", "ggplot2", "plotly", "dplyr", "DT", "stringr", "randomForest", "shinyjs", "sodium"))
+install.packages(c("shiny", "shinydashboard", "ggplot2", "plotly", "dplyr", "DT", "stringr", "randomForest", "shinyjs", "sodium", "colorspace"))
